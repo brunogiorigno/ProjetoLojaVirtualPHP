e3b911560abcf289f917bba5b1931c3622a54311
bruno giorgino <bruno.giorgino81@gmail.com>
2019-05-27 17:46:10 -0300

    Aula de loops e inclusao de form de compra

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php
 $usuario = ["logado" => true, "nome" => "Vinicius Oliveira", "nivelAcesso" => 0];
-//$usuario = "";
+
+$produtos = [ 
+	"produto1" => ["nome" => "curso1", "descrição" => "O curso 1 ensina programação", "preço" => "1400"],
+	"produto2" => ["nome" => "curso2", "descrição" => "O curso 2 ensina design", "preço" => "1100"]
+];
+
 ?>
 
 
@@ -12,8 +17,6 @@ $usuario = ["logado" => true, "nome" => "Vinicius Oliveira", "nivelAcesso" => 0]
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Loja Virtual</title>
-    <!-- -->
-    <!-- Bootstrap -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
@@ -59,37 +62,18 @@ $usuario = ["logado" => true, "nome" => "Vinicius Oliveira", "nivelAcesso" => 0]
 
     <main class="container mt-5">
         <section class="row">
-        <!-- Coluna para segurar card -->
+			
+			<?php foreach ($produtos as $chave => $produto) ?>
+			
             <div class="col-md-4">
                 <div class="card" style="width: 18rem;">
-                    <img src="..." class="card-img-top" alt="...">
+                    <img src="<?php echo $produto ["img"];  ?>" class="card-img-top" alt="...">
                     <div class="card-body">
                         <h5 class="card-title">Card title</h5>
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                         <a href="#" class="btn btn-primary">Go somewhere</a>
                     </div>
                 </div>
-            </div>
-            <div class="col-md-4">
-                <div class="card" style="width: 18rem;">
-                    <img src="..." class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="card" style="width: 18rem;">
-                    <img src="..." class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
-            </div>
         </section>
     </main>
 
